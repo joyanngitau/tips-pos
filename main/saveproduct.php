bc764c81c931673dd93d286b2af485a831619f71
JoyAnn <gitaujoyann@gmail.com>
2022-11-11 19:16:09 +0300

    still trying to fix saving a new product

--- main/saveproduct.php
+++ main/saveproduct.php
@@ -11,12 +11,12 @@ $g = $_POST['o_price'];
 $h = $_POST['profit'];
 $i = $_POST['gen'];
 $j = $_POST['date_arrival'];
-// $k = $_POST['qty_sold'];
+$k = $_POST['qty_sold'];
 
 // query
-$sql = "INSERT INTO products (product_code,product_name,expiry_date,price,supplier,qty,o_price,profit,gen_name,date_arrival) VALUES (:a,:b,:c,:d,:e,:f,:g,:h,:i,:j)";
+$sql = "INSERT INTO products (product_code,product_name,expiry_date,price,supplier,qty,o_price,profit,gen_name,date_arrival,qty_sold) VALUES (:a,:b,:c,:d,:e,:f,:g,:h,:i,:j,:k)";
 $q = $db->prepare($sql);
-$q->execute(array(':a'=>$a,':b'=>$b,':c'=>$c,':d'=>$d,':e'=>$e,':f'=>$f,':g'=>$g,':h'=>$h,':i'=>$i,':j'=>$j));
+$q->execute(array(':a'=>$a,':b'=>$b,':c'=>$c,':d'=>$d,':e'=>$e,':f'=>$f,':g'=>$g,':h'=>$h,':i'=>$i,':j'=>$j,':k'=>$k));
 header("location: products.php");
 
 
