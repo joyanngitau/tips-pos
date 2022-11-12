fc6166dcc27da748c6d6cb4bbe2739286a81f12f
JoyAnn <gitaujoyann@gmail.com>
2022-11-12 16:28:17 +0300

    fixed error that prevented checkout by adding the removed variable balance

--- main/savesales.php
+++ main/savesales.php
@@ -8,19 +8,22 @@ $d = $_POST['ptype'];
 $e = $_POST['amount'];
 $z = $_POST['profit'];
 $cname = $_POST['cname'];
+
 if($d=='credit') {
 $f = $_POST['due'];
-$sql = "INSERT INTO sales (invoice_number,cashier,date,type,amount,profit,due_date,name) VALUES (:a,:b,:c,:d,:e,:z,:f,:g)";
+$bal = $price - $e;
+$sql = "INSERT INTO sales (invoice_number,cashier,date,type,amount,profit,due_date,name,balance) VALUES (:a,:b,:c,:d,:e,:z,:f,:g,:bal)";
 $q = $db->prepare($sql);
-$q->execute(array(':a'=>$a,':b'=>$b,':c'=>$c,':d'=>$d,':e'=>$e,':z'=>$z,':f'=>$f,':g'=>$cname));
+$q->execute(array(':a'=>$a,':b'=>$b,':c'=>$c,':d'=>$d,':e'=>$e,':z'=>$z,':f'=>$f,':g'=>$cname,':bal'=>$bal));
 header("location: preview.php?invoice=$a");
 exit();
 }
 if($d=='cash') {
 $f = $_POST['cash'];
-$sql = "INSERT INTO sales (invoice_number,cashier,date,type,amount,profit,due_date,name) VALUES (:a,:b,:c,:d,:e,:z,:f,:g)";
+$bal = 0;
+$sql = "INSERT INTO sales (invoice_number,cashier,date,type,amount,profit,due_date,name,balance) VALUES (:a,:b,:c,:d,:e,:z,:f,:g,:bal)";
 $q = $db->prepare($sql);
-$q->execute(array(':a'=>$a,':b'=>$b,':c'=>$c,':d'=>$d,':e'=>$e,':z'=>$z,':f'=>$f,':g'=>$cname));
+$q->execute(array(':a'=>$a,':b'=>$b,':c'=>$c,':d'=>$d,':e'=>$e,':z'=>$z,':f'=>$f,':g'=>$cname,'bal'=>$bal));
 header("location: preview.php?invoice=$a");
 exit();
 }
